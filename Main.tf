f876e070b4697408a9a07358a1496162bfeb97e5
Atul Roy <atulroy99@gmail.com>
2024-11-19 10:35:08 +0530

    added to local extra rg

--- Main.tf
+++ Main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
     name = "rg1"
     location = "southindia"
 }
+
+resource "azurerm_resource_group" "rg2" {
+    name = "rg2"
+    location = "centralindia"
+}
